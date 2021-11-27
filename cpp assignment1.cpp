6ea5f8537729c1f9692e5143724c3ca70c26817b
swayangprangyabadu57 <91285381+swayangprangyabadu57@users.noreply.github.com>
2021-11-28 00:14:22 +0530

    Add files via upload
    C++ Program to Find Size of int, float, double and char in Your System Your output should look something like this Size of char: 1 byte Size of int: 4 bytes Size of float: 4 bytes Size of double: 8 bytes

---
+++ cpp assignment1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Size of char:" << sizeof(char) << "byte" << "\n";
+	cout << "Size of int:" << sizeof(int) << "byte" << "\n";
+	cout << "Size of float:" << sizeof(float) << "byte" << "\n";
+	cout << "Size of double:" << sizeof(double) << "byte" << "\n";
+	
+	return 0;
+}
